ba93bc2be25f66ea6f5d8a1923dca848e37f8f0f
Muvac1 <103204401+Muvac1@users.noreply.github.com>
2025-05-13 09:51:12 +0000

    wer

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ph2)i0247asscjf-t2*6ua2g=6ygse_9$ye4#v69^u-+5onlej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
